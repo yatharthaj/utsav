0b5ebe8f4a228e49308fb093732e01f05bb6c02b
Bantawa Pawan <rahunn3@gmail.com>
2018-09-13 15:06:54 +0545

    seed

--- app/Http/Composers/FrontendComposer.php
+++ app/Http/Composers/FrontendComposer.php
@@ -15,68 +15,68 @@ class FrontendComposer
 {
   public function partners(View $view)
   {
-    $view->with('partners',
-      $partners = Partner::where('status','=', 1)->get());
+    // $view->with('partners',
+    //   $partners = Partner::where('status','=', 1)->get());
   }
 
   public function menus( View $view)
   {
-    $view->with('pcategories',
-      PageCategory::all());   	
+    // $view->with('pcategories',
+    //   PageCategory::all());   	
   }
 
   public function trekRegions( View $view)
   {
-    $category = TourCategory::where('slug','=','trekking')->first();
-    $tours = $category->tours()->with('region')->get(['region_id']);
-    $regions = $tours->pluck('region')->unique();
-    $view->with('tregions',$regions);     
+    // $category = TourCategory::where('slug','=','trekking')->first();
+    // $tours = $category->tours()->with('region')->get(['region_id']);
+    // $regions = $tours->pluck('region')->unique();
+    // $view->with('tregions',$regions);     
   }
 
   public function climbRegions( View $view)
   {
-    $category = TourCategory::where('slug','=','climbing')->first();
-    $tours = $category->tours()->with('region')->get(['region_id']);
-    $regions = $tours->pluck('region')->unique();
-    $view->with('cregions',$regions);     
+    // $category = TourCategory::where('slug','=','climbing')->first();
+    // $tours = $category->tours()->with('region')->get(['region_id']);
+    // $regions = $tours->pluck('region')->unique();
+    // $view->with('cregions',$regions);     
   }  
 
   public function skis( View $view)
   {
-    $category = TourCategory::where('slug','=','ski')->first();
-    $tours = $category->tours()->with('region')->get(['region_id']);
-    $regions = $tours->pluck('region')->unique();
-    $view->with('skis',$regions);     
+    // $category = TourCategory::where('slug','=','ski')->first();
+    // $tours = $category->tours()->with('region')->get(['region_id']);
+    // $regions = $tours->pluck('region')->unique();
+    // $view->with('skis',$regions);     
   }  
 
   public function tours( View $view)
   {
-    $category = TourCategory::where('slug','=','ski')->first();
-    $tours = $category->tours()->with('region')->get(['region_id']);
-    $regions = $tours->pluck('region')->unique();
-    $view->with('tours',$regions);     
+    // $category = TourCategory::where('slug','=','ski')->first();
+    // $tours = $category->tours()->with('region')->get(['region_id']);
+    // $regions = $tours->pluck('region')->unique();
+    // $view->with('tours',$regions);     
   }   
   public function countries( View $view)
   {
-    $countries = Country::all();
-    $view->with('fcountries',$countries);     
+    // $countries = Country::all();
+    // $view->with('fcountries',$countries);     
   } 
 
   public function pages( View $view)
   {
-    $pages = Page::where('status',1)->get();
-    $view->with('fpages',$pages);     
+    // $pages = Page::where('status',1)->get();
+    // $view->with('fpages',$pages);     
   }  
 
   public function activity( View $view)
   {
-    $activity = TourCategory::all();
-    $view->with('factivities',$activity);     
+    // $activity = TourCategory::all();
+    // $view->with('factivities',$activity);     
   }    
 
   public function contact(View $view)
   {
-    $contact = ContactDetail::first();
-    $view->with('fcontact', $contact);
+    // $contact = ContactDetail::first();
+    // $view->with('fcontact', $contact);
   }
 }
