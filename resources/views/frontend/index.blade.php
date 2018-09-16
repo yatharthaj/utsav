5bae29eec70c1a3af2d6ff844c3248f3895d5fae
Bantawa Pawan <rahunn3@gmail.com>
2018-09-16 11:14:41 +0545

    slides

--- resources/views/frontend/index.blade.php
+++ resources/views/frontend/index.blade.php
@@ -13,7 +13,7 @@
         data-slotamount="7" data-easein="default" data-easeout="default" data-masterspeed="1000"
         data-rotate="0" data-saveperformance="off" data-title="Slide" data-description="">
         <!-- main image for revolution slider -->
-        <img src="{{ {{ asset($slide->path) }}" alt="image description" data-bgposition="center center"
+        <img src="{{ asset($slide->path) }}" alt="image description" data-bgposition="center center"
         data-kenburns="on" data-duration="30000" data-ease="Linear.easeNone"
         data-scalestart="100" data-scaleend="120" data-rotatestart="0" data-rotateend="0"
         data-offsetstart="0 0" data-offsetend="0 0" data-bgparallax="10" class="rev-slidebg"
@@ -39,7 +39,7 @@
         data-splitout="none"
         data-responsive_offset="on"
         style="z-index: 7; letter-spacing: 0; font-weight: 100; text-align: center; color: #ffffff">
-        {{ $slde->header }}
+        {{ $slide->header }}
     </div>
 
     <div class="tp-caption banner-heading-sub tp-resizeme rs-parallaxlevel-10"
