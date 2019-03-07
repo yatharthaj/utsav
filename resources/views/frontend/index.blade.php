925b7600100e7f854c4f3153ab0aed3f0e10a974
Bantawa Pawan <rahunn3@gmail.com>
2019-03-07 14:29:13 +0545

    home

--- resources/views/frontend/index.blade.php
+++ resources/views/frontend/index.blade.php
@@ -1,16 +1,14 @@
 @extends('layouts.frontend')
-@section('content')
-@if(!empty($slides))
+@section('content') @if(!empty($slides))
 <div class="owl-carousel owl-theme home-slider">
     @foreach($slides as $slide)
-    <img class="item responsive-img" src="{{ asset($slide->path) }}" alt="">
-    @endforeach
+    <img class="item responsive-img" src="{{ asset($slide->path) }}" alt=""> @endforeach
 </div>
 @endif
 
 <!-- heading start -->
 <div class="container-fluid heading-wrapper">
-    <h3 class="center-align heading-title">SPLASH RESPONSIBLE TRAVEL ALL OVER</h3>
+    <h3 class="center-align heading-title">YOUR VENTURE LEADS TO ADVENTURE</h3>
     <p class="center-align uk-margin-medium-bottom heading-para"><i>Striving to be the Nepal's only most Eco-Friendly Ski
     Touring Experience !</i></p>
 </div>
@@ -20,24 +18,31 @@
     <div class="row symbol">
         <div class="col s12 m4 l4 center-align feature-wrapper">
             <div class="featured-sustain"></div>
-            <h5 class="center-align">Sustainable Tourism</h5>
-            <p class="center-align">We believe a small effort towards the betterment of community can contribute a
-                lot to environment. As a responsible operator we strongly emphasis on cultural and economic
-            sustainability.</p>
+            <h5 class="center-align">Sustainable</h5>
+            <p class="center-align">
+                We always support sustainable development to protect our nature by using degradable product, planting trees and local product.
+                We believe in working directly with local guide and people and protect the beauty of Nepal for hundreds of
+                years.
+            </p>
 
         </div>
         <div class="col s12 m4 l4 center-align feature-wrapper">
             <div class="featured-adventure"></div>
-            <h5 class="center-align">Adventure Tourism</h5>
-            <p class="center-align">We try to go extra miles & redefine the dimension of adventure tourism in Nepal.
-            We bing the trend of the outdoors ski tourism in Nepal.</p>
+            <h5 class="center-align">Safety and Quality</h5>
+            <p class="center-align">
+                We think safety and quality is the key for successful journey. We always try to keep our guest in good condition in every
+                means physically and mentally by providing our best hospitality in mountains with the help of our best guide
+                and cook. Safety first is our main motto.
+            </p>
 
         </div>
         <div class="col s12 m4 l4 center-align feature-wrapper">
             <div class="featured-ski"></div>
-            <h5 class="center-align">Authentic Ski Tours</h5>
-            <p class="center-align">We believe tourism should be experience locally.
-            We offered Ski touring in the landscape of High Altitude Himalayas.</p>
+            <h5 class="center-align">Social Impact</h5>
+            <p class="center-align">
+                We are main sponsor to Charity organization Ski and Snowboarding Foundation Nepal which promote winter sport in Nepal. We
+                provide 30% of our profit to this organization in order to develop Ski/snowboard culture among Nepalese.
+            </p>
         </div>
 
     </div>
@@ -45,16 +50,15 @@
 <div class="container-fluid trip-wrapper">
     <div class="row">
         <div class="col s12 m6 l6 cat">
-            @include('frontend.partials._tripadvisor')
+    @include('frontend.partials._tripadvisor')
         </div>
         <div class="col s12 m6 l6 intro Uk-margin-remove">
             <h3 class="center-align">Crafted Ski Tours - Lasting Memories</h3>
-            <p>We seek to promote the skiing culture in Nepal in days to come. apart from mainstream treks, Skiing
-                Is complete new recreation activity in Nepal however, we have witnessed professional skiers who
-                successfully accomplished skiing activity in the Himalayas. Nepal and its Himalayas have good
-                potential to attract passionate travelers, who wish to hit slopes of Nepal once in a lifetime.
-                Realizing The possibility of new adventure sports to the Native land of Himalayas, Ski Guides Nepal
-            Is Formed.</p>
+            <p>We seek to promote the skiing culture in Nepal in days to come. apart from mainstream treks, Skiing Is complete
+                new recreation activity in Nepal however, we have witnessed professional skiers who successfully accomplished
+                skiing activity in the Himalayas. Nepal and its Himalayas have good potential to attract passionate travelers,
+                who wish to hit slopes of Nepal once in a lifetime. Realizing The possibility of new adventure sports to
+                the Native land of Himalayas, Ski Guides Nepal Is Formed.</p>
             <div class="center-align">
                 <a class="waves-effect waves-light btn-large intro-button" href="/about/hear-our-story">Read more..</a>
             </div>
@@ -65,8 +69,7 @@
 <!-- Special block start -->
 {{-- @if(!empty($offer->message)) --}}
 <div class="parallax-container center valign-wrapper" id="offer">
-    <div class="parallax"><img
-        src="{{asset('img/banner/annapurna.jpg')}}">
+    <div class="parallax"><img src="{{asset('img/banner/annapurna.jpg')}}">
     </div>
 
     <div class="container white-text">
@@ -91,7 +94,7 @@
         <div class="col s12 m4 l4">
             <div class="card">
                 <div class="card-image">
-                     <a href="{{ route('frontend-tourDetail',[$tour->slug]) }}"><img src="{{$tour->featuredImage->thumbnail}}" class="responsive-img"></a>
+                    <a href="{{ route('frontend-tourDetail',[$tour->slug]) }}"><img src="{{$tour->featuredImage->thumbnail}}" class="responsive-img"></a>
                     <div class="featured-price white-text">
                         <span><sup>USD</sup>
                             @if(!empty($tour->price2))
@@ -102,9 +105,8 @@
                     </div>
                 </div>
                 <div class="card-content package-meta">
-                    <a href="{{ route('frontend-tourDetail',[$tour->slug]) }}"
-                     class="card-title uk-margin-remove-vertical" id="package-title">{{$tour->title}}</a>
-                     <div class="unit">
+                    <a href="{{ route('frontend-tourDetail',[$tour->slug]) }}" class="card-title uk-margin-remove-vertical" id="package-title">{{$tour->title}}</a>
+                    <div class="unit">
                         <span class="left"><i class="far fa-calendar-alt"></i> {{$tour->days}} Days</span>
                         <span class="rating-wrapper right">
                             <i class="material-icons review-star">star</i>
@@ -120,8 +122,7 @@
         @endforeach
     </div>
 </div>
-{{-- trip of the month start --}}
-@if(!empty($month))
+{{-- trip of the month start --}} @if(!empty($month))
 <section class="tofm-wrapper">
     <div class="container-fluid tofm-wrap ">
         <div class="row center-align heading-wrapper">
@@ -129,8 +130,7 @@
         </div>
         <div class="row">
             <div class="col s12 m6 l6 uk-padding-removel" id="tofm-img-wrapper">
-                <img class="responsive-img" src="{{asset($month->tour->featuredImage->path)}}"
-                alt="{{$month->tour->title}}">
+                <img class="responsive-img" src="{{asset($month->tour->featuredImage->path)}}" alt="{{$month->tour->title}}">
             </div>
             <div class="col s12 m6 l6">
                 <h3 class="center-align">{{$month->tour->title}}</h3>
@@ -178,8 +178,7 @@
                             </div>
                         </div>
                         <div class="center-align heading-button uk-padding-large">
-                            <a class="waves-effect waves-light btn btn-large tofm-meta-wrapper-buttom"
-                            href="{{ route('frontend-tourDetail',[$month->tour->slug]) }}">Discover</a>
+                            <a class="waves-effect waves-light btn btn-large tofm-meta-wrapper-buttom" href="{{ route('frontend-tourDetail',[$month->tour->slug]) }}">Discover</a>
                         </div>
 
                     </div>
@@ -188,10 +187,7 @@
         </div>
     </div>
 </section>
-@endif
-{{-- trip of the month end --}}
-
-{{-- youtube,instagram start--}}
+@endif {{-- trip of the month end --}} {{-- youtube,instagram start--}}
 <section class="social">
     <div class="row social-row">
         <div class="col s12 m6 l6 uk-padding-remove heading-wrapper social-col" id="insta-wrapper">
@@ -200,7 +196,7 @@
             <div class="row insta-row">
                 @foreach($row as $feed)
                 <div class="col s4 m4 l4">
-                <a href="{{$feed->link}}" target="_blank">
+                    <a href="{{$feed->link}}" target="_blank">
                             <img class="responsive-img" src="{{$feed->thumb}}" alt="{{$feed->caption}}">
                     </a>
                 </div>
@@ -212,14 +208,12 @@
         <div class="col s12 m6 l6 heading-wrapper " id="youtube-wrapper">
             <h3 class="center-align heading-title"> Ski Guides Nepal Stories</h3>
             <div class="video-container">
-                <iframe width="853" height="640" src="https://www.youtube.com/embed/7ZXWtxc5Xxk" frameborder="0"
-                allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
-                allowfullscreen></iframe>
+                <iframe width="853" height="640" src="https://www.youtube.com/embed/7ZXWtxc5Xxk" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
+                    allowfullscreen></iframe>
             </div>
         </div>
     </div>
 </section>
 {{-- youtube,instagram end--}}
-@include('frontend.partials._media')
+    @include('frontend.partials._media')
 @stop
-
